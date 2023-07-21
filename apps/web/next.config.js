eff1a1c6831bd02499c1ba6467771c44d0e0366b
Jonathan <jonathangus@proton.me>
2023-07-21 22:35:45 +0200

    wip

--- apps/web/next.config.js
+++ apps/web/next.config.js
@@ -1,5 +1,6 @@
 module.exports = {
   reactStrictMode: false,
+  transpilePackages: ['ui', 'shared-types'],
 
   typescript: {
     // !! WARN !!
--- apps/web/src/pages/api/convert.ts
+++ apps/web/src/pages/api/convert.ts
@@ -1,13 +1,44 @@
-// Next.js API route support: https://nextjs.org/docs/api-routes/introduction
 import type { NextApiRequest, NextApiResponse } from 'next';
+import { createPublicClient, http } from 'viem';
+import { polygonMumbai } from 'viem/chains';
+import { UserObject, PostObject } from 'shared-types';
 
-type Data = {
-  name: string;
-};
+const transport = http(
+  `https://polygon-mainnet.g.alchemy.com/v2/${process.env.ALCHEMY_KEY}`
+);
 
-export default function handler(
+const client = createPublicClient({
+  chain: polygonMumbai,
+  transport,
+});
+
+export default async function handler(
   req: NextApiRequest,
-  res: NextApiResponse<Data>
+  res: NextApiResponse<any>
 ) {
-  res.status(200).json({ name: 'John Doe' });
+  const { user, posts } = req.body as {
+    posts: PostObject[];
+    user: UserObject;
+  };
+
+  // await contract.createUser()
+  const userData = {
+    handle: user.handle,
+    imageURI: user.imageURI,
+    followModule: '0x...',
+    followModuleInitData: '0x',
+    followNFTURI: '',
+  };
+
+  const postsData = posts.map((post) => ({
+    contentURI: post.contentURI,
+    collectModule: '0x00',
+    collectModuleInitData: '0x',
+    referenceModule: '0x00',
+    referenceModuleInitData: '0x',
+  }));
+
+  const blockNumber = await client.getBlockNumber();
+
+  res.status(200).json({ name: 'John Doe', br: Number(blockNumber) });
 }
