3994a3f7f26eec9e823739715c99fd8a2aa918e3
Morkeeth <42701407+Morkeeth@users.noreply.github.com>
2023-07-22 13:16:28 +0200

    Images and description post
    update images, titles and sources

--- apps/web/src/mocked/mocked-items.ts
+++ apps/web/src/mocked/mocked-items.ts
@@ -1,50 +1,65 @@
 import { PostEntity } from 'shared-types';
 import image1 from './image1.png';
+import image2 from './image2.png';
+import image3 from './image3.png';
+import image4 from './image4.png';
+import image5 from './image5.png';
+import image6 from './image6.png';
+
 
 export const mockedItems: PostEntity[] = [
   {
     image: image1.src,
     title:
-      'brand identity lorem brand identity lorem brand identity lorem brand identity',
-    handle: 'mock_dribble.test.lens',
+      'Lenstagram brand identity',
+    handle: 'heavydevbehance.lens',
     mocked: true,
-    postId: 'asd',
+    postId: '1',
+    service: 'Behance',
+  },
+  {
+    image: image2.src,
+    title:
+      'Train ride galore WebGL',
+    handle: 'gruvanspline.lens',
+    mocked: true,
+    postId: '2',
+    service: 'spline',
+  },
+  {
+    image: image3.src,
+    title:
+      'onchain summer LENSTHREADS',
+    handle: 'zuckyberrydribble.lens',
+    mocked: true,
+    postId: '3',
     service: 'dribbble',
   },
   {
-    image: image1.src,
+    image: image4.src,
     title:
-      'brand identity lorem brand identity lorem brand identity lorem brand identity',
-    handle: 'mock_dribble.test.lens',
+      'NOUNS ⌐◨-◨ AI concept',
+    handle: 'Weirdcreatordribbble.lens',
     mocked: true,
-    postId: 'asd',
+    postId: '4',
     service: 'dribbble',
   },
   {
-    image: image1.src,
+    image: image5.src,
     title:
-      'brand identity lorem brand identity lorem brand identity lorem brand identity',
-    handle: 'mock_dribble.test.lens',
+      'Yerbie framer motion animation',
+    handle: 'Gerbitcodepen.lens',
     mocked: true,
-    postId: 'asd',
-    service: 'dribbble',
+    postId: '5',
+    service: 'Codepen',
   },
   {
-    image: image1.src,
+    image: image6.src,
     title:
-      'brand identity lorem brand identity lorem brand identity lorem brand identity',
-    handle: 'mock_dribble.test.lens',
+      'Windows98 CSS Library',
+    handle: 'Mrballmercodebox.lens',
     mocked: true,
-    postId: 'asd',
-    service: 'dribbble',
-  },
-  {
-    image: image1.src,
-    title:
-      'brand identity lorem brand identity lorem brand identity lorem brand identity',
-    handle: 'mock_dribble.test.lens',
-    mocked: true,
-    postId: 'asd',
-    service: 'dribbble',
+    postId: '6',
+    service: 'codebox',
   },
 ];
