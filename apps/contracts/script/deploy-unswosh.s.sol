aee41038492eeb13d0c7a7cc627e1729017c3794
Pilou <76021631+0xPilou@users.noreply.github.com>
2023-07-22 11:20:43 +0200

    script

--- apps/contracts/script/deploy-unswosh.s.sol
+++ apps/contracts/script/deploy-unswosh.s.sol
@@ -1,7 +1,7 @@
 // SPDX-License-Identifier: MIT
 pragma solidity ^0.8.0;
 
-import "forge-std/Script.sol";
+import {Script} from "forge-std/Script.sol";
 
 import {UnswoshNFT} from "src/UnswoshNFT.sol";
 
--- apps/contracts/script/get-profile-data.s.sol
+++ apps/contracts/script/get-profile-data.s.sol
@@ -2,10 +2,11 @@
 pragma solidity ^0.8.0;
 
 import {Script} from "forge-std/Script.sol";
+import "forge-std/console.sol";
 
 import {UnswoshNFT} from "src/UnswoshNFT.sol";
 
-contract MintProfile is Script {
+contract GetProfileData is Script {
     UnswoshNFT public unswoshNFT;
 
     function run() external {
@@ -18,15 +19,9 @@ contract MintProfile is Script {
         // Deploy Implementation Contracts
         unswoshNFT = UnswoshNFT(0x3e85C2aEC80C2B84FF05e08FBD827C4fCaC9FD6c);
 
-        UnswoshNFT.PartialCreateProfileData memory data = UnswoshNFT.PartialCreateProfileData(
-            "pierre-dribble",
-            "https://ipfs.io/ipfs/QmcfP6PSQFzxMYkCZY88VtR5TZcq58gvQg6PTpL5DvNrk2",
-            0x0000000000000000000000000000000000000000,
-            "0x",
-            "ipfs://QmRQ38pPu99Znd9jjQ1gUeSN6G8w5M2spQA7z2nNSs3rh6"
-        );
-
-        unswoshNFT.mintProfile(0xAd88438F0DF2939e383648D7d2c783C47086A5e6, data);
+        console.log("UnswoshNFT address : %s", unswoshNFT.accountsPerTokenId(0));
+        console.log("Owner address: %s", unswoshNFT.ownerOf(0));
+        console.log("ProfileId : %s", unswoshNFT.profileIdPerTokenId(0));
 
         vm.stopBroadcast();
     }
