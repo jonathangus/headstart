caa2960be50161b64bf78f51d897aebd753985fc
Morkeeth <42701407+Morkeeth@users.noreply.github.com>
2023-07-22 15:37:42 +0200

    content update 2
    more img, more content

--- apps/web/src/mocked/mocked-items.ts
+++ apps/web/src/mocked/mocked-items.ts
@@ -9,7 +9,13 @@ import image7 from './image7.png';
 import image8 from './image8.png';
 import image9 from './image9.png';
 import image10 from './image10.png';
-
+import image11 from './image11.png';
+import image12 from './image12.png';
+import image13 from './image13.png';
+import image14 from './image14.png';
+import image15 from './image15.png';
+import image16 from './image16.png';
+import image17 from './image17.png';
 
 
 
@@ -87,68 +93,68 @@ export const mockedItems: PostEntity[] = [
     service: 'dribbble',
   },
   {
-    image: image4.src,
+    image: image10.src,
     title: 'NOUNS ⌐◨-◨ AI concept',
     handle: 'Weirdcreatordribbble.lens',
     mocked: true,
-    postId: '4',
+    postId: '10',
     service: 'dribbble',
   },
   {
-    image: image5.src,
-    title: 'Yerbie framer motion animation',
-    handle: 'Gerbitcodepen.lens',
+    image: image11.src,
+    title: 'Illustration on deviant',
+    handle: 'paintitdeviant.lens',
     mocked: true,
-    postId: '5',
-    service: 'Codepen',
+    postId: '11',
+    service: 'Deviant',
   },
   {
-    image: image6.src,
-    title: 'Windows98 CSS Library',
-    handle: 'Mrballmercodebox.lens',
+    image: image12.src,
+    title: 'particule WebGL',
+    handle: 'Atmospherecodebox.lens',
     mocked: true,
-    postId: '6',
+    postId: '12',
     service: 'codebox',
   },
   {
-    image: image10.src,
-    title: 'How do I delete a Git branch locally and remotely?',
-    handle: 'MatthewSO.lens',
+    image: image13.src,
+    title: 'letter swirl',
+    handle: 'marbleandacidcodesandbox.lens',
     mocked: true,
-    postId: '10',
+    postId: '13',
+    service: 'Codesandbox',
+  },
+  {
+    image: image14.src,
+    title: 'Interactive poster designer',
+    handle: 'spraynprayCS.lens',
+    mocked: true,
+    postId: '14',
+    service: 'Codesandbox',
+  },
+  {
+    image: image15.src,
+    title: 'Looksrare NFT design',
+    handle: 'Elementalsandbeansbehance.lens',
+    mocked: true,
+    postId: '15',
+    service: 'behance',
+  },
+  {
+    image: image16.src,
+    title: 'How do I check if an element is hidden in jQuery?',
+    handle: 'PhilipSO.lens',
+    mocked: true,
+    postId: '17',
     service: 'Stackoverflow',
   },
   {
-    image: image2.src,
-    title: 'Train ride galore WebGL',
-    handle: 'gruvanspline.lens',
+    image: image17.src,
+    title: 'blackcoffee unreleased demo',
+    handle: 'blackcoffesounds.lens',
     mocked: true,
-    postId: '2',
-    service: 'spline',
-  },
-  {
-    image: image3.src,
-    title: 'onchain summer LENSTHREADS',
-    handle: 'zuckyberrydribble.lens',
-    mocked: true,
-    postId: '3',
-    service: 'dribbble',
-  },
-  {
-    image: image4.src,
-    title: 'NOUNS ⌐◨-◨ AI concept',
-    handle: 'Weirdcreatordribbble.lens',
-    mocked: true,
-    postId: '4',
-    service: 'dribbble',
-  },
-  {
-    image: image5.src,
-    title: 'Yerbie framer motion animation',
-    handle: 'Gerbitcodepen.lens',
-    mocked: true,
-    postId: '5',
-    service: 'Codepen',
+    postId: '19',
+    service: 'soundcloud',
   },
   {
     image: image6.src,
