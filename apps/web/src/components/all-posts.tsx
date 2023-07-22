dfbc5ba89fa9f2b908a525bf43b62438e05d78b8
Jonathan <jonathangus@proton.me>
2023-07-22 18:46:14 +0200

    all posts

--- apps/web/src/components/all-posts.tsx
+++ apps/web/src/components/all-posts.tsx
@@ -1,27 +1,15 @@
-<<<<<<< HEAD
 import { usePostsQuery } from '@/hooks/use-posts-query';
 import { PostsList } from './posts-list';
 import { LensPost, PostEntity } from 'shared-types';
-=======
-import { usePostsQuery } from "@/hooks/use-posts-query";
-import { PostsList } from "./posts-list";
->>>>>>> 7fae713f5fd7ae3809f5d5e27a743184f7817e5a
 
 type Props = {
   realPosts: PostEntity[];
 };
 
-<<<<<<< HEAD
 export function AllPosts({ realPosts }: Props) {
   const { data = [], isLoading } = usePostsQuery();
 
   const items = [...realPosts, ...data];
-=======
-  if (isLoading) {
-    return <div>loading...</div>;
-  }
-
->>>>>>> 7fae713f5fd7ae3809f5d5e27a743184f7817e5a
   return (
     <div>
       <PostsList items={items} />
